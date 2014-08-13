394ed2b2a560e9d4bfcf9e85f3b7311f100f6801
Sterling Hanenkamp <hanenkamp@cpan.org>
2014-08-13 09:47:17 -0500

    Adding documentation

--- lib/Bot/Backbone/Service/Web/LinkTitle.pm
+++ lib/Bot/Backbone/Service/Web/LinkTitle.pm
@@ -2,6 +2,8 @@ package Bot::Backbone::Service::Web::LinkTitle;
 use v5.10;
 use Bot::Backbone::Service;
 
+# ABSTRACT: Retrieve the titles of links pasted into chat
+
 with qw(
     Bot::Backbone::Service::Role::Service
     Bot::Backbone::Service::Role::Responder
@@ -10,9 +12,45 @@ with qw(
 use URI::Find;
 use URI::Title qw( title );
 
+=head1 SYNOPSIS
+
+    service link_titles => (
+        service => 'Web::LinkTitle'
+    );
+
+    # in chat
+    alice> https://metacpan.org/release/Acme-Fork-Bomb
+    bot> Acme-Fork-Bomb-2.0 - crashes your program and probably your system - metacpan.org
+
+=head1 DESCRIPTION
+
+Whenever someone pasts a link to a chat the bot is monitoring, the bot will use
+L<URI::Title> to find the title or description of the linked document and report
+it back to the chat.
+
+=head1 DISPATCHER
+
+This monitors all chats and looks for links in them. It locates URLs in the
+messages using L<URI::Find>. When a link is found, it is checked using
+L<URI::Title> and whatever that module finds is reported to the chat.
+
+=cut
+
 service_dispatcher as {
     also not_command respond_by_method 'describe_links';
 };
+
+=head1 ATTRIBUTES
+
+=head2 exclude_urls
+
+This is a list of regular expressions used to identify URLs you want to exclude
+from being checked. For example, you might frequently link to an internal site
+that requires a login that the bot does not have access to. You can keep it from
+trying to report the link titles of those, which would otherwise just be noise
+like "Please login".
+
+=cut
 
 has exclude_urls => (
     is          => 'rw',
@@ -25,6 +63,14 @@ has exclude_urls => (
     },
 );
 
+=head1 METHODS
+
+=head2 excluded_url
+
+Checks to see if a URL matches any of the regexes in L</exclude_urls>.
+
+=cut
+
 sub excluded_url {
     my ($self, $url) = @_;
 
@@ -34,6 +80,12 @@ sub excluded_url {
 
     return '';
 }
+
+=head2 find_links
+
+Helper that finds URLs in text using L<URI::Find>.
+
+=cut
 
 sub find_links {
     my ($self, $text) = @_;
@@ -51,6 +103,13 @@ sub find_links {
     return @links;
 }
 
+=head2 describe_links
+
+Looks at every chat message and searches it for URLs to fetch the
+title/description and reports the title/description back to the chat.
+
+=cut
+
 sub describe_links {
     my ($self, $message) = @_;
 
@@ -62,6 +121,12 @@ sub describe_links {
 
     return @messages;
 }
+
+=head2 initialize
+
+No op.
+
+=cut
 
 sub initialize { }
 
